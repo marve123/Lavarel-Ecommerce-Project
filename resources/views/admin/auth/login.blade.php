09e4bc4480117e89fa0184e2e7f8b0b2d3d60049
marve123 <eric.marvelboy@gmail.com>
2024-01-19 02:35:35 +0100

    frontend-setup-one

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 <h1>Admin Login</h1>
@@ -26,9 +26,15 @@
 
         <!-- Remember Me -->
         <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            <label for="remember_me" class="inline-flex
+            items-center">
+                <input id="remember_me" type="checkbox"
+                class="rounded border-gray-300
+                text-indigo-600 shadow-sm
+                focus:ring-indigo-500" name="remember">
+                <span class="ms-2 text-sm
+                text-gray-600">{{ __('Remember me') }}
+            </span>
             </label>
         </div>
 
@@ -44,10 +50,7 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
-
-
-
+</x-guest-layout> --}}
 
 <!DOCTYPE html>
 <html lang="en">
@@ -84,7 +87,7 @@
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+              <img src="{{asset('backend/assets/img/stisla-fill.svg')}}" alt="logo" width="100" class="shadow-light rounded-circle">
             </div>
 
             <div class="card card-primary">
@@ -93,14 +96,19 @@
               <div class="card-body">
                 <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
                   @csrf
+
+                   <!-- Email Address -->
                     <div class="form-group">
                     <label for="email">Email</label>
-                    <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
-                    <div class="invalid-feedback">
-                      Please fill in your email
-                    </div>
+                    <input id="email" type="email" class="form-control"
+                    name="email" tabindex="1" required autofocus
+                    value="{{old('email')}}">
+                   @if ($errors->has('email'))
+                        <code>{{$errors->first('email')}}</code>
+                   @endif
                   </div>
 
+            <!-- Password -->
                   <div class="form-group">
                     <div class="d-block">
                     	<label for="password" class="control-label">Password</label>
@@ -110,48 +118,34 @@
                         </a>
                       </div>
                     </div>
-                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
-                    <div class="invalid-feedback">
-                      please fill in your password
-                    </div>
+                    <input id="password" type="password" class="form-control"
+                    name="password" tabindex="2" required>
+                    @if ($errors->has('password'))
+                    <code>{{$errors->first('password')}}</code>
+                    @endif
                   </div>
 
+
+                <!-- Remember Me -->
                   <div class="form-group">
                     <div class="custom-control custom-checkbox">
-                      <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
-                      <label class="custom-control-label" for="remember-me">Remember Me</label>
+                      <input type="checkbox" name="remember"
+                      class="custom-control-input" tabindex="3"
+                      id="remember-me">
+                      <label class="custom-control-label" for="remember-me">
+                        Remember Me</label>
                     </div>
                   </div>
 
                   <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block"
+                    tabindex="4">
                       Login
                     </button>
                   </div>
                 </form>
-                <div class="text-center mt-4 mb-3">
-                  <div class="text-job text-muted">Login With Social</div>
-                </div>
-                <div class="row sm-gutters">
-                  <div class="col-6">
-                    <a class="btn btn-block btn-social btn-facebook">
-                      <span class="fab fa-facebook"></span> Facebook
-                    </a>
-                  </div>
-                  <div class="col-6">
-                    <a class="btn btn-block btn-social btn-twitter">
-                      <span class="fab fa-twitter"></span> Twitter
-                    </a>
-                  </div>
-                </div>
 
               </div>
-            </div>
-            <div class="mt-5 text-muted text-center">
-              Don't have an account? <a href="auth-register.html">Create One</a>
-            </div>
-            <div class="simple-footer">
-              Copyright &copy; Stisla 2018
             </div>
           </div>
         </div>
--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -4,7 +4,7 @@
         <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
         <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
       </ul>
-      
+
     </form>
     <ul class="navbar-nav navbar-right">
       <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link nav-link-lg message-toggle beep"><i class="far fa-envelope"></i></a>
@@ -137,7 +137,7 @@
         <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a>
         <div class="dropdown-menu dropdown-menu-right">
           <div class="dropdown-title">Logged in 5 min ago</div>
-          <a href="features-profile.html" class="dropdown-item has-icon">
+          <a href="{{route('admin.profile')}}" class="dropdown-item has-icon">
             <i class="far fa-user"></i> Profile
           </a>
           <a href="features-activities.html" class="dropdown-item has-icon">
@@ -147,9 +147,16 @@
             <i class="fas fa-cog"></i> Settings
           </a>
           <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item has-icon text-danger">
-            <i class="fas fa-sign-out-alt"></i> Logout
-          </a>
+
+           <!-- Authentication -->
+           <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <a href="{{route('logout')}}" onclick="event.preventDefault();
+             this.closest('form').submit();"
+            class="dropdown-item has-icon text-danger">
+              <i class="fas fa-sign-out-alt"></i> Logout
+            </a>
+        </form>
         </div>
       </li>
     </ul>
